0316c0dea78eaa7fe11072f997c17ec5f882dedb
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 13:34:24 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,26 @@
+public class Operators {
+    
+        public static void main (String[]args){
+            int a = 20;
+            int b = 8;
+    
+            System.out.println(a); 
+            System.out.printf("the value of a is %d \n",a);
+            System.out.println(b);
+            System.out.printf("the value of b is %d \n",b);
+            System.out.println(a+b);
+            System.out.printf("the sum of %d + %d is %d \n",a,b,a+b);
+            System.out.println(a-b);
+            System.out.printf("the sum of %d  - %d is %d \n",a,b,a-b);
+            System.out.println(a*b);
+            System.out.printf("the sum of %d  mlt by %d is %d \n",a,b,a*b);
+            System.out.println(a/b);
+            System.out.printf("the sum of %d div %d is %d \n ",a,b,a/b);
+            System.out.println(a%b);
+            System.out.printf("the sum of %d mod %d is %d \n ",a,b,a%b);
+    
+        }
+        
+    }
+    
+
